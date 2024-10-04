f827dbfef0f5c8ed5c742017069f62df2426b031
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-10-03 22:52:21 -0300

    initial commit

---
+++ src/main/java/com/outercode/UpLocacoes/UpLocacoesApplication.java
@@ -0,0 +1,13 @@
+package com.outercode.UpLocacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpLocacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpLocacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outercode/UpLocacoes/UpLocacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outercode.UpLocacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpLocacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
