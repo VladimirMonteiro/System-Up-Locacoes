2ce3e37b83170cdd3c9aef9b9c93fb3e44e136af
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-06 21:30:17 -0300

    create state repository

---
+++ src/main/java/com/outercode/UpLocacoes/repositories/StateRepository.java
@@ -0,0 +1,7 @@
+package com.outercode.UpLocacoes.repositories;
+
+import com.outercode.UpLocacoes.models.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Integer> {
+}
