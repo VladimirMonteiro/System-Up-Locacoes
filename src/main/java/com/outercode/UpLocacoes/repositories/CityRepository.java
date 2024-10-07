ca5fe8a68a1757168c0dc140f4a2cd302aaa3c39
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-06 21:24:46 -0300

    create city repository

---
+++ src/main/java/com/outercode/UpLocacoes/repositories/CityRepository.java
@@ -0,0 +1,7 @@
+package com.outercode.UpLocacoes.repositories;
+
+import com.outercode.UpLocacoes.models.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Integer> {
+}
