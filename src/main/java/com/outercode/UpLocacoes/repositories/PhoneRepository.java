0379b8b64aac418b4028ac30dbb4e60023c264cc
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-06 21:28:42 -0300

    create phone repository

---
+++ src/main/java/com/outercode/UpLocacoes/repositories/PhoneRepository.java
@@ -0,0 +1,7 @@
+package com.outercode.UpLocacoes.repositories;
+
+import com.outercode.UpLocacoes.models.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhoneRepository extends JpaRepository<Phone, Integer> {
+}
