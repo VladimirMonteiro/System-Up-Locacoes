ef1428afb0c4f8c18ca3088edd0762cab956f848
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-06 21:31:05 -0300

    create tool repository

---
+++ src/main/java/com/outercode/UpLocacoes/repositories/ToolRepository.java
@@ -0,0 +1,7 @@
+package com.outercode.UpLocacoes.repositories;
+
+import com.outercode.UpLocacoes.models.Tool;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToolRepository extends JpaRepository<Tool, Integer> {
+}
