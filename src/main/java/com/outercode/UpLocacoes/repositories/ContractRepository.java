d9cf0d3f251b986e87bf1fc0c6fbfb090527d796
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-06 21:26:05 -0300

    create Contract repository

---
+++ src/main/java/com/outercode/UpLocacoes/repositories/ContractRepository.java
@@ -0,0 +1,7 @@
+package com.outercode.UpLocacoes.repositories;
+
+import com.outercode.UpLocacoes.models.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRepository extends JpaRepository<Contract, Integer> {
+}
